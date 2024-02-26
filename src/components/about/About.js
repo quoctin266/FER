c7f37b85625199ad3c1eb0e34fa61fb669667cb2
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-02-26 21:06:41 +0700

    .

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,10 +3,61 @@ import Accordion from "react-bootstrap/Accordion";
 import { HiUsers } from "react-icons/hi";
 import { MdLocationOn } from "react-icons/md";
 import { BiNews } from "react-icons/bi";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { toast } from "react-toastify";
+import { postUploadChunk } from "../../service/fileService";
 
 const About = () => {
+  const [file, setFile] = useState(null);
+
+  const handleChangeFile = (file) => {
+    console.log(file);
+    setFile(file);
+  };
+
+  const handleUpload = async () => {
+    if (file) {
+      // let res = await postUploadFile(file, "video", "demo");
+      // if (res.status === 201) toast.success(res.message);
+
+      const CHUNK_SIZE = 1000 * 1000 * 50;
+      const totalChunks =
+        file.size % CHUNK_SIZE === 0
+          ? file.size / CHUNK_SIZE
+          : Math.floor(file.size / CHUNK_SIZE) + 1;
+
+      const fileName = file.name;
+
+      await Promise.all(
+        Array.apply(null, Array(totalChunks)).map(async (item, index) => {
+          const CHUNK = file.slice(
+            index * CHUNK_SIZE,
+            (index + 1) * CHUNK_SIZE
+          );
+
+          return postUploadChunk(CHUNK, fileName, totalChunks);
+        })
+      );
+    } else toast.error("no file chosen");
+  };
+
   return (
     <div className="about-container">
+      <Form.Control
+        className="mb-3"
+        type="file"
+        onChange={(e) => handleChangeFile(e.target.files[0])}
+      />
+      <Button
+        className="mb-3"
+        variant="contained"
+        onClick={() => handleUpload()}
+      >
+        Upload
+      </Button>
+
       <Accordion defaultActiveKey={["0"]} alwaysOpen>
         <Accordion.Item eventKey="0">
           <Accordion.Header>
--- src/service/fileService.js
+++ src/service/fileService.js
@@ -11,8 +11,17 @@ const postUploadFile = async (file, folder_type, folder_name) => {
   });
 };
 
+const postUploadChunk = async (chunk, fileName, totalChunks) => {
+  const payload = new FormData();
+  payload.append("chunk", chunk);
+  payload.append("fileName", fileName);
+  payload.append("totalChunks", totalChunks);
+
+  return await axios.post("/files/chunk", payload);
+};
+
 const removeFile = async (fileName) => {
   return await axios.delete("files", { headers: { fileName } });
 };
 
-export { postUploadFile, removeFile };
+export { postUploadFile, removeFile, postUploadChunk };
